3c5e5e5c22440d06d13ebae0ff1afbcc25e88fd9
Kenan Berker <40179214+kbberker@users.noreply.github.com>
2025-05-28 09:33:32 +0100

    feat: add @notionhq/client dependency and initialize Notion client in the server

--- packages/notion-todo-server/src/index.ts
+++ packages/notion-todo-server/src/index.ts
@@ -1,13 +1,20 @@
+import { Client } from "@notionhq/client";
 import { Hono } from "hono";
 
 type Bindings = {
   SECRET_KEY: string;
+  NOTION_TOKEN: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/", (c) => {
-  console.log(c.env.SECRET_KEY);
+  // Initializing a client
+  const notion = new Client({
+    auth: c.env.NOTION_TOKEN,
+  });
+
+  console.log(c.env.NOTION_TOKEN);
   return c.text("Hello Hono!");
 });
 
