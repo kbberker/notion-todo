567087a393a1341cc607922a086744e0342e7ea3
Kenan Berker <40179214+kbberker@users.noreply.github.com>
2025-07-09 21:34:53 +0100

    feat: group tasks by status group and status option

--- apps/notion-todo-client/src/features/TodoList.tsx
+++ apps/notion-todo-client/src/features/TodoList.tsx
@@ -1,12 +1,73 @@
 import { useQuery } from "@tanstack/react-query";
-import type { ApiDatabaseResponse } from "nt-types";
+import type {
+  ApiDatabaseResponse,
+  StatusDatabasePropertyConfigResponse,
+} from "nt-types";
 import { Route } from "../routes/tasks.$dbId";
+import { useGetDatabases } from "../hooks/useGetDatabses";
+
+type GroupedTasks = {
+  [groupId: string]: {
+    groupName: string;
+    options: {
+      [optionId: string]: {
+        optionName: string;
+        tasks: ApiDatabaseResponse["results"];
+      };
+    };
+  };
+};
+
+function groupTasksByStatus(
+  tasks: ApiDatabaseResponse["results"],
+  statusProperties: StatusDatabasePropertyConfigResponse
+) {
+  const groupedTasks = tasks.reduce((acc: GroupedTasks, task) => {
+    const taskStatusObj = Object.values(task.properties).find(
+      (property) => property.type === "status"
+    );
+
+    const taskStatus = taskStatusObj?.status;
+
+    if (!taskStatusObj || !taskStatus) {
+      return acc; // Skip tasks without a status
+    }
+
+    // Each task's status' ID will belong to one of the status groups
+    const taskStatusGroup = statusProperties.status.groups.find((group) =>
+      group.option_ids.includes(taskStatus.id)
+    );
+
+    if (taskStatusGroup?.id) {
+      if (!acc[taskStatusGroup.id]) {
+        acc[taskStatusGroup.id] = {
+          groupName: taskStatusGroup.name,
+          options: {},
+        };
+      }
+      const optionId = taskStatus.id;
+      const optionName = taskStatus.name;
+      if (!acc[taskStatusGroup.id].options[optionId]) {
+        acc[taskStatusGroup.id].options[optionId] = {
+          optionName,
+          tasks: [],
+        };
+      }
+
+      acc[taskStatusGroup.id].options[optionId].tasks.push(task);
+    }
+
+    return acc;
+  }, {} as GroupedTasks);
+
+  return groupedTasks;
+}
 
 export const TodoList = () => {
   const { dbId } = Route.useParams();
-
-  const { data, isLoading } = useQuery({
-    queryKey: ["todos"],
+  const { data: databases } = useGetDatabases();
+  const { data: tasksData, isLoading } = useQuery({
+    queryKey: ["tasks", dbId],
     queryFn: async () => {
       const response = await fetch(
         `http://localhost:8787/api/databases/${dbId}/tasks`
@@ -19,9 +80,27 @@ export const TodoList = () => {
     },
   });
 
-  console.log({ data });
+  const properties = databases?.results.find(
+    (db) => db.id === dbId
+  )?.properties;
 
-  const tasks = data?.results.map((task) => {
+  const statusProperties = Object.values(properties || {}).filter(
+    (property) => property.type === "status"
+  );
+
+  console.log({ statusProperty: statusProperties });
+
+  console.log({ data: tasksData });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!tasksData || tasksData.results.length === 0) {
+    return <div>No tasks found.</div>;
+  }
+
+  const tasks = tasksData?.results.map((task) => {
     const name = Object.values(task.properties).find(
       (property) => property.type === "title"
     )?.title[0]?.plain_text;
@@ -29,15 +108,11 @@ export const TodoList = () => {
     return name;
   });
 
-  console.log({ tasks });
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!tasks || tasks.length === 0) {
-    return <div>No tasks found.</div>;
-  }
+  const groupedTasks = groupTasksByStatus(
+    tasksData?.results,
+    statusProperties[0]
+  );
+  console.log({ groupedTasks });
 
   return (
     <div>
--- packages/nt-types/src/api/index.ts
+++ packages/nt-types/src/api/index.ts
@@ -12,3 +12,38 @@ export type ApiDatabaseResponse = Omit<QueryDatabaseResponse, "results"> & {
 export type DatabaseSearchResponse = Omit<SearchResponse, "results"> & {
   results: DatabaseObjectResponse[];
 };
+
+type SelectColor =
+  | "default"
+  | "gray"
+  | "brown"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "purple"
+  | "pink"
+  | "red";
+
+type StatusPropertyResponse = {
+  id: string;
+  name: string;
+  color: SelectColor;
+  description: string | null;
+};
+
+export type StatusDatabasePropertyConfigResponse = {
+  type: "status";
+  status: {
+    options: Array<StatusPropertyResponse>;
+    groups: Array<{
+      id: string;
+      name: string;
+      color: SelectColor;
+      option_ids: Array<string>;
+    }>;
+  };
+  id: string;
+  name: string;
+  description: string | null;
+};
