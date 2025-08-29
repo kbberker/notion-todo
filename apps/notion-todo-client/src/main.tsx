1cf598b55dacd5ccb68aa38ebe94a8454f32eead
Kenan Berker <kbbuk90@gmail.com>
2025-08-29 20:58:31 +0100

    chore: update biome config and fix issues

--- apps/notion-todo-client/src/main.tsx
+++ apps/notion-todo-client/src/main.tsx
@@ -1,20 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { routeTree } from "./routeTree.gen";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
 
+// biome-ignore lint/style/noNonNullAssertion: defined inside index.html
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 );
--- packages/notion-todo-server/src/index.ts
+++ packages/notion-todo-server/src/index.ts
@@ -1,8 +1,8 @@
 import type { SearchParameters } from "@notionhq/client/build/src/api-endpoints";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import type { HonoBindings } from "./types";
 import { databases } from "./routes/databases";
+import type { HonoBindings } from "./types";
 
 const app = new Hono<{ Bindings: HonoBindings }>();
 
@@ -12,30 +12,28 @@ app.use("*", cors());
 app.route("/api/databases", databases);
 
 app.get("/search", async (c) => {
-  const id = c.req.param("id");
+	const searchParams: SearchParameters = {
+		filter: {
+			property: "object",
+			value: "database",
+		},
+	};
 
-  const searchParams: SearchParameters = {
-    filter: {
-      property: "object",
-      value: "database",
-    },
-  };
+	const response = await fetch(`https://api.notion.com/v1/search`, {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${c.env.NOTION_TOKEN}`,
+			"Notion-Version": "2022-06-28",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(searchParams),
+	});
 
-  const response = await fetch(`https://api.notion.com/v1/search`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${c.env.NOTION_TOKEN}`,
-      "Notion-Version": "2022-06-28",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(searchParams),
-  });
+	const listUsersResponse = await response.json();
 
-  const listUsersResponse = await response.json();
+	console.log(listUsersResponse);
 
-  console.log(listUsersResponse);
-
-  return c.text("Hello Hono!");
+	return c.text("Hello Hono!");
 });
 
 export default app;
