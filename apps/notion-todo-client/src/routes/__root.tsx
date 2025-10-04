ac755aa99d62cfc33c47a87063889a7baecffe80
Kenan Berker <kbbuk90@gmail.com>
2025-10-04 14:41:12 +0100

    chore: update dependencies and add react-router-devtools

--- apps/notion-todo-client/src/routes/__root.tsx
+++ apps/notion-todo-client/src/routes/__root.tsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const queryClient = new QueryClient();
 
 function RootComponent() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Outlet />
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
   );
 }
 
