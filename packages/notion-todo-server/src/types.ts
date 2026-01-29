ca5073bd3e800b08d3975eeb547d5f0d6ac38c54
Kenan Berker <kbbuk90@gmail.com>
2026-01-29 21:27:23 +0000

    chore: add Notion OAuth configuration to HonoBindings type

--- packages/notion-todo-server/src/types.ts
+++ packages/notion-todo-server/src/types.ts
@@ -1,5 +1,10 @@
 export type HonoBindings = {
   SECRET_KEY: string;
   NOTION_TOKEN: string;
+  NOTION_OAUTH_CLIENT_ID: string;
+  NOTION_OAUTH_CLIENT_SECRET: string;
+  NOTION_AUTH_URL: string;
+  NOTION_REDIRECT_URI: string;
+  USER_SESSION: KVNamespace;
   ENVIRONMENT: "development" | "production";
 };
